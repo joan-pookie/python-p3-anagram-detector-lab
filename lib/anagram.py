a56ace89fdc690dc1e052e33c934af86150f6934
joan <joan92311@gmail.com>
2025-08-17 08:39:40 +0300

    anagram detector

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, word_list):
+        matches = []
+        for candidate in word_list:
+            if sorted(candidate.lower()) == sorted(self.word) and candidate.lower() != self.word:
+                matches.append(candidate)
+        return matches
